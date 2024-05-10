5d959e464e220a23cb4a3d8495b37aabb7b17d6c
Hybeee <szcsa2003@gmail.com>
2024-05-10 08:53:07 +0200

    isolation + test cases in a txt file

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,32 +8,42 @@ import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
+  private TorpedoStore mockPrimary;
+  private TorpedoStore mockSecondary;
   private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockPrimary = mock(TorpedoStore.class);
+    mockSecondary = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockPrimary, mockSecondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockPrimary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
+    verify(mockPrimary, times(1)).fire(1);
     assertEquals(true, result);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockPrimary.fire(1)).thenReturn(true);
+    when(mockSecondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
+    verify(mockPrimary, times(1)).fire(1);
+    verify(mockSecondary, times(1)).fire(1);
     assertEquals(true, result);
   }
 
